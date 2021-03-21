ffb995b42f89cb9a20ba1d8e17c42498aadefc09
Francois Gutierrez <gutijeanf@gmail.com>
2021-03-21 23:01:05 +0100

    Cros update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require ('cors');
 
 var app = express();
 const CURRENT_WORKING_DIR = process.cwd();
@@ -20,6 +21,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/index.routes'));
