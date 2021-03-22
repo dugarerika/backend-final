81d61004e61c40d272689171be6bbe11b252dbb1
Alvagit25 <alvarodesegovia@gmail.com>
2021-03-22 13:35:48 +0100

    "poner email"

--- app.js
+++ app.js
@@ -1,62 +1,97 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require ('cors');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var nodemailer = require("nodemailer");
+var cors = require("cors");
 
 var app = express();
 const CURRENT_WORKING_DIR = process.cwd();
 
 //Solucionamos problema de CORS
-app.use(cors({credentials: true, origin:true}));
+app.use(cors({ credentials: true, origin: true }));
 
 // conectar a la base de datos
-require('./lib/connectMongoose');
-app.use(
-	'/public',
-	express.static(path.join(CURRENT_WORKING_DIR, 'public'))
-);
+require("./lib/connectMongoose");
+app.use("/public", express.static(path.join(CURRENT_WORKING_DIR, "public")));
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', require('./routes/index.routes'));
-app.use('/users', require('./routes/users.routes'));
-app.use('/', require('./routes/user.routes'));
-app.use('/', require('./routes/auth.routes'));
-app.use('/', require('./routes/shop.routes'));
-app.use('/', require('./routes/product.routes'));
-app.use('/', require('./routes/order.routes'));
+app.use("/", require("./routes/index.routes"));
+app.use("/users", require("./routes/users.routes"));
+app.use("/", require("./routes/user.routes"));
+app.use("/", require("./routes/auth.routes"));
+app.use("/", require("./routes/shop.routes"));
+app.use("/", require("./routes/product.routes"));
+app.use("/", require("./routes/order.routes"));
 
+app.post("/api/form", (req, res) => {
+  const data = req.body;
 
+  const smtpTransport = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    /* service: "Gmail", */
+    port: 465,
+    service: "gmail",
+    auth: {
+      user: " ",
+      pass: " ",
+    },
+  });
 
+  const mailOptions = {
+    from: `${data.email}`,
+    to: "alvarodesegovia@gmailcom",
+    replyTo: "elbarenal@gmail.com",
+    subject: `${data.asunto}`,
+    /* text: req.body.mensaje, */
+    html: htmlEmail,
+  };
 
+  const htmlEmail = `
+	  <h3>Email enviado desde Wallarock</h3>
+	  <ul>
+	  <li>Email: ${data.email}</li>
+	  <li>Asunto: ${data.asunto}</li>
+	  </ul>
+	  <h3>Mensaje</h3>
+	  <p>${data.mensaje}</p>
+	  `;
+
+  transporter.sendMail(mailOptions, (err, res) => {
+    if (err) {
+      res.send(err);
+    } else {
+      console.log("mensaje enviado");
+    }
+  });
+
+  smtpTransport.close();
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-	next(createError(404));
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error =
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-			req.app.get('env') === 'development' ? err :
-			{};
-
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;
